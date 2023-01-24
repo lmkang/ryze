e346e06a979f5dc41f29a7c14ed64a7ae3b10ced
lmkang25@163.com <lmkang25@163.com>
2023-01-25 02:08:56 +0800

    update

--- server/http_parser.c
+++ server/http_parser.c
@@ -7,8 +7,7 @@ struct http_request_line {
     size_t method_len;
     char *path;
     size_t path_len;
-    char *version;
-    size_t version_len;
+    int minor_version;
 };
 
 struct http_header {
@@ -18,40 +17,12 @@ struct http_header {
     size_t value_len;
 };
 
-struct http_resolved_header {
-    size_t content_length;
-    int transfer_encoding;
-    int connection;
-};
-
-static int http_strcmp_ignore(const char *data, size_t data_len, 
-        const char *value, size_t value_len) {
-    if(data_len != value_len) {
-        return 0;
-    }
-    int ret = 1;
-    int d1, d2, d3;
-    for(size_t i = 0; i < data_len; i++) {
-        d1 = data[i] - value[i];
-        if(diff == 0) {
-            continue;
-        }
-        d2 = data[i] - 'a';
-        d3 = data[i] - 'A';
-        if(d2 >= 0 && d2 <= 25 || d3 >= 0 && d3 <= 25) {
-            continue;
-        }
-        ret = 0;
-        break;
-    }
-    return ret;
-}
-
-static size_t http_parse_int(const char *data, size_t data_len, int radix) {
-    size_t result = 0;
+static int str_parse_int(const char *data, size_t data_len, int radix, size_t *result) {
+    *result = 0;
+    int ret = 0;
     size_t base = 1;
-    int n = 0;
     size_t i = data_len - 1;
+    int n = 0;
     while(i >= 0) {
         if(data[i] >= '0' && data[i] <= '9') {
             n = data[i] - '0';
@@ -59,19 +30,23 @@ static size_t http_parse_int(const char *data, size_t data_len, int radix) {
             n = data[i] - 'a' + 10;
         } else if(data[i] >= 'A' && data[i] <= 'Z') {
             n = data[i] - 'A' + 10;
+        } else {
+            ret = -1;
+            break;
         }
-        result += n * base;
+        *result += n * base;
         if(i == 0) {
             break;
         }
         i--;
         base *= radix;
     }
-    return result;
+    return ret;
 }
 
 int http_parse_request_line(const char *data, size_t data_len, 
         struct http_request_line *request_line, size_t *read_len) {
+    *read_len = 0;
     char *pos[3];
     int j = 0;
     for(size_t i = 0; i < data_len && j < 3; i++) {
@@ -80,12 +55,22 @@ int http_parse_request_line(const char *data, size_t data_len,
         }
     }
     if(j == 3) {
+        if(pos[2] - data + 1 == data_len) {
+            return 1;
+        }
+        if(*(pos[2] + 1) != '\n' || pos[2] - pos[1] != 9) {
+            return -1;
+        }
+        char *p = pos[1];
+        if(*++p != 'H' || *++p != 'T' || *++p != 'T' || *++p != 'P' 
+                || *++p != '/' || *++p != '1' || *++p != '.') {
+            return -1;
+        }
         request_line->method = (char *) data;
         request_line->method_len = pos[0] - request_line->method;
         request_line->path = pos[0] + 1;
         request_line->path_len = pos[1] - request_line->path;
-        request_line->version = pos[1] + 1 + 5;
-        request_line->version_len = pos[2] - request_line->version;
+        request_line->minor_version = *(pos[1] + 8) - '0';
         *read_len = pos[2] + 1 - data + 1;
         return 0;
     }
@@ -93,69 +78,65 @@ int http_parse_request_line(const char *data, size_t data_len,
 }
 
 int http_parse_header(const char *data, size_t data_len, 
-        struct http_header *headers, size_t max_header, size_t *nheader, 
-        struct http_header_info *header_info, size_t *read_len) {
-    header_info->content_length = 0;
-    header_info->connection = kKeepAlive;
-    header_info->transfer_encoding = kNone;
-    for(size_t i = 0, j = 0, index = 0; i < data_len - 4; i++) {
-        if(data[i] == '\r' && data[i + 1] == '\n') {
-            *nheader += 1;
-            *read_len = i + 2;
-            if(index == max_header) {
-                max_header = (max_header * 3) >> 2;
-                headers = realloc(headers, max_header);
-            }
-            for(size_t k = j; k < i; k++) {
-                if(data[k] == ':') {
-                    headers[index].name = (char *) data + j;
-                    headers[index].name_len = k - j;
-                    while(data[++k] == ' ' || data[k] == '\t') {
-                        // skip spaces or tabs
-                    }
-                    headers[index].value = (char *) data + k;
-                    headers[index].value_len = i - k;
-                    if(http_strcmp_ignore(headers[index].name, 
-                        headers[index].name_len, "Content-Length", 14)) {
-                        header_info->content_length = http_parse_int(
-                            headers[index].value, headers[index].value_len, 10);
-                    }
-                    if(http_strcmp_ignore(headers[index].name, 
-                        headers[index].name_len, "Transfer-Encoding", 17)) {
-                        if(http_strcmp_ignore(headers[index].value, 
-                            headers[index].value_len, "chunked", 7)) {
-                            header_info->transfer_encoding = kChunked;
-                        }
-                    }
-                    if(http_strcmp_ignore(headers[index].name, 
-                        headers[index].name_len, "Connection", 10)) {
-                        if(http_strcmp_ignore(headers[index].value, 
-                            headers[index].value_len, "close", 7)) {
-                            header_info->connection = kClose;
-                        }
-                    }
-                    break;
+        struct http_header *headers, size_t max_header, 
+        size_t *nheader, size_t *read_len) {
+    *nheader = 0;
+    *read_len = 0;
+    char *p1 = (char *) data, *p2 = NULL, *p3 = NULL, *p4 = NULL;
+    struct http_header *h;
+    for(size_t i = 0, j = 0; i < data_len; i++) {
+        if(j == max_header) {
+            return 2;
+        }
+        if(data[i] == ':') {
+            p2 = (char *) data + i;
+        } else if(p2 != NULL && p3 == NULL && data[i] != ' ' && data[i] != '\t') {
+            p3 = (char *) data + i;
+        } else if(data[i] == '\r' && i + 1 < data_len && data[i + 1] == '\n') {
+            if(p2 != NULL && p3 != NULL && p4 != NULL) {
+                h = &headers[j];
+                h->name = p1;
+                h->name_len = p2 - p1;
+                h->value = p3;
+                h->value_len = p4 + 1 - p3;
+                *nheader = ++j;
+            } else if((*p1 == ' ' || *p1 == '\t') && j > 0) {
+                // line folding, skip SP and HT, concat value
+                char *p = p1 + 1;
+                while(*p == ' ' || *p == '\t') {
+                    p++;
+                }
+                int len = data + i - p;
+                if(len > 0) {
+                    h = &headers[j - 1];
+                    memmove(h->value + h->value_len, p, len);
+                    h->value_len += len;
                 }
             }
-            j = i + 2;
-            index++;
-            if(data[i + 2] == '\r' && data[i + 3] == '\n') {
-                *read_len += 2;
+            if(i + 3 < data_len && data[i + 2] == '\r' && data[i + 3] == '\n') {
+                *read_len = i + 2 + 2;
                 return 0;
             }
+            *read_len = i + 2;
+            p1 = (char *) data + i + 2;
+            p2 = NULL;
+            p3 = NULL;
+            p4 = NULL;
             i++;
+        } else if(p3 != NULL && data[i] != ' ' && data[i] != '\t') {
+            p4 = (char *) data + i;
         }
     }
     return 1;
 }
 
-int http_parse_body(const char *data, size_t data_len, 
-        size_t content_len, int is_chunked, size_t *read_len) {
+int http_parse_chunked(const char *data, size_t data_len, size_t *read_len) {
     return 0;
 }
 
 int main(int argc, char **argv) {
-    const char *data = "POST /aa/bb/cc HTTP/1.1\r\nContent-Length: 11\r\n\r\nhello world";
+    char *data = malloc(4096);
+    strcpy(data, "POST /aa/bb/cc HTTP/1.1\r\nContent-Length: 1234\r\nX-Test11: aa\r\n bb\r\nX-Test22: cc, dd, ee \r\n\r\nhello world");
     size_t data_len = strlen(data);
     struct http_request_line *request_line = malloc(sizeof(struct http_request_line));
     size_t read_len = 0;
@@ -163,15 +144,14 @@ int main(int argc, char **argv) {
     if(ret == 0) {
         printf("method: %.*s\n", request_line->method_len, request_line->method);
         printf("path: %.*s\n", request_line->path_len, request_line->path);
-        printf("version: %.*s\n", request_line->version_len, request_line->version);
+        printf("minor version: %d\n", request_line->minor_version);
         
-        size_t max_header = 16;
+        size_t max_header = 64;
         struct http_header *headers = malloc(sizeof(struct http_header) * max_header);
-        struct http_header_info *header_info = malloc(sizeof(struct http_header_info));
         size_t nheader = 0;
         size_t read_len2 = 0;
         ret = http_parse_header(data + read_len, data_len - read_len, 
-            headers, max_header, &nheader, header_info, &read_len2);
+            headers, max_header, &nheader, &read_len2);
         if(ret == 0) {
             printf("num of header: %d\n", nheader);
             printf("headers : \n");
@@ -180,13 +160,12 @@ int main(int argc, char **argv) {
                     headers[i].name_len, headers[i].name, 
                     headers[i].value_len, headers[i].value);
             }
-            printf("content length: %d\n", header_info->content_length);
-            printf("chunked: %d\n", http_is_chunked(header_info));
-            printf("keep alive: %d\n", http_is_keep_alive(request_line, header_info));
+        } else {
+            printf("fail to parse header: %d\n", ret);
         }
         free(headers);
-        free(header_info);
     }
     free(request_line);
+    free(data);
     return 0;
 }
