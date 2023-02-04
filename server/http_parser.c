93f329aef3ef3e12890943d2e9497eadf80e5f7d
lmkang25@163.com <lmkang25@163.com>
2023-02-04 23:26:21 +0800

    update

--- server/http_parser.c
+++ server/http_parser.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "http_parser.h"
+#include <stdio.h>
 
 static const char *token_char_map = 
     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
@@ -36,34 +37,38 @@ int http_parse_request_line(char *data, size_t data_len,
         int *minor_version, size_t *read_len) {
     *read_len = 0;
     char *pos[3];
-    int j = 0;
-    for(size_t i = 0; i < data_len; i++) {
-        if(data[i] == ' ') {
-            pos[j++] = data + i;
-        } else if(data[i] == '\r') {
-            if(j == 2 && i + 1 < data_len && data[i + 1] == '\n') {
-                pos[j++] = data + i;
+    char *p = data, *end = data + data_len;
+    int i = 0;
+    for(; p != end; p++) {
+        if(*p == ' ') {
+            pos[i++] = p;
+            if(i == 2) {
                 break;
-            } else {
-                return -1;
             }
         }
     }
-    if(j == 3) {
-        if(pos[2] - pos[1] != 9) {
+    for(; p != end; p++) {
+        if(*p == '\r' && ++p != end && *p == '\n') {
+            pos[i++] = p;
+            break;
+        }
+    }
+    if(i == 3) {
+        // " HTTP/1.1\r\n"
+        if(pos[2] - pos[1] != 10) {
             return -1;
         }
         char *p = pos[1];
-        if(p[1] != 'H' || p[2] != 'T' || p[3] != 'T' || p[4] != 'P' 
-                || p[5] != '/' || p[6] != '1' || p[7] != '.') {
+        if(*++p != 'H' || *++p != 'T' || *++p != 'T' || *++p != 'P' 
+                || *++p != '/' || *++p != '1' || *++p != '.') {
             return -1;
         }
         *method = data;
         *method_len = pos[0] - *method;
         *path = pos[0] + 1;
         *path_len = pos[1] - *path;
-        *minor_version = *(pos[1] + 8) - '0';
-        *read_len = pos[2] + 1 - data + 1;
+        *minor_version = *++p - '0';
+        *read_len = pos[2] - data + 1;
         return 0;
     }
     return 1;
@@ -74,61 +79,70 @@ int http_parse_header(char *data, size_t data_len,
         int *nheader, size_t *read_len) {
     *nheader = 0;
     *read_len = 0;
+    char *p = data, *end = data + data_len;
+    char *p1 = data, *p2 = NULL, *p3 = NULL, *t;
     struct http_header *h;
-    char *p, *p1 = data, *p2 = NULL;
-    int n, is_token;
-    for(size_t i = 0, j = 0; i < data_len; i++) {
-        if(data[i] == ':') {
-            p2 = data + i;
-        } else if(data[i] == '\r' && i + 1 < data_len && data[i + 1] == '\n') {
-            if(p1[0] != ' ' && p1[0] != '\t') {
-                if(p2 != NULL) {
-                    if(j == max_header) {
-                        return 2;
-                    }
-                    // token = 1*<any CHAR except CTLs or separators>
-                    is_token = 1;
-                    for(n = 0; n < p2 - p1; n++) {
-                        if(!token_char_map[(unsigned char) p1[n]]) {
-                            is_token = 0;
-                            break;
-                        }
-                    }
-                    if(is_token) {
-                        h = &headers[j];
-                        h->name = p1;
-                        h->name_len = p2 - p1;
-                        p = p2 + 1;
-                        while(*p == ' ' || *p == '\t') {
-                            p++;
-                        }
-                        h->value = p;
-                        p = data + i - 1;
-                        while(*p == ' ' || *p == '\t') {
-                            p--;
-                        }
-                        h->value_len = p + 1 - h->value;
-                        *nheader = ++j;
-                    }
+    int i = 0, is_token;
+    for(; p != end;) {
+        t = p1;
+        is_token = 1;
+        for(;; t++) {
+            // token = 1*<any CHAR except CTLs or separators>
+            if(!token_char_map[(unsigned char) *t]) {
+                if(*t == ':') {
+                    p2 = t;
+                    break;
+                } else {
+                    is_token = 0;
                 }
-            } else if(j > 0) {
+            }
+        }
+        for(;; t++) {
+            if(*t == '\r' && ++t != end && *t == '\n') {
+                p3 = t;
+                break;
+            }
+        }
+        p = t;
+        if(p3) {
+            if(*p1 != ' ' && *p1 != '\t') {
+                if(is_token && p2 && i < max_header) {
+                    h = &headers[i];
+                    h->name = p1;
+                    h->name_len = p2 - p1;
+                    t = p2 + 1;
+                    while(*t == ' ' || *t == '\t') {
+                        t++;
+                    }
+                    h->value = t;
+                    t = p3 - 2;
+                    while(*t == ' ' || *t == '\t') {
+                        t--;
+                    }
+                    h->value_len = t + 1 - h->value;
+                    *nheader = ++i;
+                }
+            } else if(i > 0) {
                 // line folding
-                p = p1 + 1;
-                int len = data + i - p;
+                t = p1 + 1;
+                int len = p - 1 - t;
                 if(len > 0) {
-                    h = &headers[j - 1];
-                    memmove(h->value + h->value_len, p, len);
+                    h = &headers[i - 1];
+                    memmove(h->value + h->value_len, t, len);
                     h->value_len += len;
                 }
             }
-            if(i + 3 < data_len && data[i + 2] == '\r' && data[i + 3] == '\n') {
-                *read_len = i + 2 + 2;
-                return 0;
+            t = ++p;
+            if(t != end) {
+                if(*t == '\r' && ++p != end && *p == '\n') {
+                    *read_len = p - data + 1;
+                    return 0;
+                }
+                p1 = t;
+                p2 = NULL;
+                p3 = NULL;
+                *read_len = t - data;
             }
-            *read_len = i + 2;
-            p1 = data + i + 2;
-            p2 = NULL;
-            i++;
         }
     }
     return 1;
